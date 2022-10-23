74a2c3cb79367d0b6b2b13693fda520bc36d6f51
Stanislavych <stasik.bartosh@mail.ru>
2022-10-24 00:22:27 +0300

    delete commit for id

--- Flights.Model/Models/Airport.cs
+++ Flights.Model/Models/Airport.cs
@@ -7,7 +7,7 @@ namespace Flights.Model.Models
     [Table("Airports")]
     public class Airport
     {
-        public int Id { get; set; } // id
+        public int Id { get; set; }
         public string Address { get; set; } = string.Empty;
     }
 }
